de4f6cf4a53a65dde47266dc474f99de0296bd93
Sidhant Sharma <sidhant0312@gmail.com>
2017-10-12 23:39:01 -0400

    added more test endpoints

--- api/app/views.py
+++ api/app/views.py
@@ -1,6 +1,7 @@
 #This file contains all the routes for our application.
 
 from app import app
+from flask import request 
 
 @app.route('/')
 def index():
@@ -9,3 +10,11 @@ def index():
 @app.route('/route/<testvariable>')
 def testFunction(testvariable):
 	return "You passed in the variable %s" % testvariable
+
+@app.route('/post/<int:id>')
+def testIntFunction(id):
+	return "You passed in the integer %d" % id
+
+@app.route('/getme', methods=["POST"])
+def getHttpTest():
+	return "GET test"
