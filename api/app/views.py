cb11af545ae3115114927dac2201acb3c5eb4f88
Sidhant Sharma <sidhant0312@gmail.com>
2017-10-16 20:48:54 -0400

    testing mysql from flask

--- api/app/views.py
+++ api/app/views.py
@@ -3,9 +3,18 @@
 from app import app
 from flask import request 
 
+from flaskext.mysql import MySQL
+
+mysql = MySQL()
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'Sabu12345'
+app.config['MYSQL_DATABASE_DB'] = 'EmpData'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+
 @app.route('/')
 def index():
-    return 'Hello Worlddd!'
+	return 'Hello Worlddd!'
 
 @app.route('/route/<testvariable>')
 def testFunction(testvariable):
@@ -18,3 +27,16 @@ def testIntFunction(id):
 @app.route('/getme', methods=["POST"])
 def getHttpTest():
 	return "GET test"
+
+@app.route("/Authenticate")
+def Authenticate():
+	username = request.args.get('UserName')
+	password = request.args.get('Password')
+	cursor = mysql.connect().cursor()
+	cursor.execute("SELECT * from User where Username='" + username + "' and Password='" + password + "'")
+	data = cursor.fetchone()
+	if data is None:
+		return "Username or Password is wrong"
+	else:
+		return "Logged in successfully"	
+
