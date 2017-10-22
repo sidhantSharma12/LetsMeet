04866eade4942a99facf155c145289e2ac336aed
Sidhant Sharma <sidhant0312@gmail.com>
2017-10-21 23:59:34 -0400

    sample input

--- project/App.js
+++ project/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default class App extends React.Component {
    constructor(props) {
     super(props);
-    this.state = {showText: ''};
+    this.state = {showText: '', text:''};
   }
   componentWillMount() {
     return fetch('http://127.0.0.1:5000/Authenticate')
@@ -21,6 +21,14 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <TextInput
+          style={{height: 40}}
+          placeholder="Type here to translate!"
+          onChangeText={(text) => this.setState({text})}
+        />
+         <Text style={{padding: 10, fontSize: 42}}>
+          {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
+        </Text>
         <Text>{this.state.showText}</Text>
       </View>
     );
