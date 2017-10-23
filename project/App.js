aba2007ed9ef3b6a901095930a8d959ee236b839
Sidhant Sharma <sidhant0312@gmail.com>
2017-10-22 22:23:03 -0400

    Added navigation

--- project/App.js
+++ project/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StackNavigator } from 'react-navigation';
 
-export default class App extends React.Component {
-   constructor(props) {
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Wellccome'
+  };
+
+  constructor(props) {
     super(props);
     this.state = {showText: '', text:''};
   }
+
   componentWillMount() {
     return fetch('http://127.0.0.1:5000/Authenticate')
       .then((response) => response.json())
@@ -18,7 +24,12 @@ export default class App extends React.Component {
       });
   }
 
+  login(){
+    console.log('hi') 
+  }
+
   render() {
+    const { navigate } = this.props.navigation;
     return (
       <View style={styles.container}>
         <TextInput
@@ -30,7 +41,39 @@ export default class App extends React.Component {
           {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
         </Text>
         <Text>{this.state.showText}</Text>
+        <Button onPress={this.login} title="Login" color="#841584"/>
+        <Button onPress={this.signup} title="Signup" color="#841584"/>
+        <Button onPress={() => navigate('Chat')} title="Chat with Lucy"/>
       </View>
+    );
+  }
+}
+
+class ChatScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Chat with Lucy',
+  };
+  render() {
+    return (
+      <View>
+        <Text>Hello Text</Text>
+      </View>
+    );
+  }
+}
+
+
+const SimpleApp = StackNavigator({
+  Home: { screen: HomeScreen },
+  Chat: { screen: ChatScreen }
+});
+
+export default class App extends React.Component {
+
+  render() {
+    return (
+      /**/
+      <SimpleApp />
     );
   }
 }
