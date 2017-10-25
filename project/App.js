e810db6e8042de260c6b914a567a67a4d1a68e36
Sidhant Sharma <sidhant0312@gmail.com>
2017-10-24 23:08:49 -0400

    added google maps and marker

--- project/App.js
+++ project/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 import { StackNavigator, TabNavigator } from 'react-navigation';
+import MapView from 'react-native-maps';
 
 class RecentChatsScreen extends React.Component {
   render() {
@@ -10,7 +11,30 @@ class RecentChatsScreen extends React.Component {
 
 class AllContactsScreen extends React.Component {
   render() {
-    return <Text>List of all contacts</Text>
+    return (
+          <View style={styles.container}> 
+            <MapView
+              initialRegion={{
+                latitude: 37.78825,
+                longitude: -122.4324,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+              }}
+              style={styles.map}>
+                <MapView.Marker 
+                  coordinate={{
+                    latitude: 37.78825,
+                    longitude: -122.4324
+                  }}
+                >
+                  <View style={styles.radius}>
+                    <View style={styles.marker}/>
+                  </View>
+                </MapView.Marker>
+
+            </MapView>
+          </View>
+          );
   }
 }
 
@@ -99,4 +123,31 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  map: {
+    left:0,
+    right:0,
+    top:0,
+    bottom:0,
+    position:'absolute'
+  },
+  radius:{
+    height:50,
+    width:50,
+    borderRadius:50/2,
+    overflow:'hidden',
+    backgroundColor: 'rgba(0,122,255, 0.1)',
+    borderWidth: 1,
+    borderColor:'rgba(0,122,255, 0.3)',
+    alignItems:'center',
+    justifyContent: 'center'
+  },
+  marker:{
+    height:20,
+    width:20,
+    borderWidth:3,
+    borderColor:'white',
+    borderRadius:20/2,
+    overflow:'hidden',
+    backgroundColor:'#007AFF'
+  }
 });
